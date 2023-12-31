413c1b899976188377ab6610002a5a1234d40aff
Wildan Kurnia Candra <37010270+user4xn@users.noreply.github.com>
2023-12-31 19:14:41 +0700

    Search

--- src/views/utama/VerifikasiPemilih.vue
+++ src/views/utama/VerifikasiPemilih.vue
@@ -120,7 +120,7 @@
         <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M5.94118 10.7474V20.7444C5.94118 21.0758 5.81103 21.3936 5.57937 21.628C5.3477 21.8623 5.0335 21.994 4.70588 21.994H2.23529C1.90767 21.994 1.59347 21.8623 1.36181 21.628C1.13015 21.3936 1 21.0758 1 20.7444V11.997C1 11.6656 1.13015 11.3477 1.36181 11.1134C1.59347 10.879 1.90767 10.7474 2.23529 10.7474H5.94118ZM5.94118 10.7474C7.25166 10.7474 8.50847 10.2207 9.43512 9.28334C10.3618 8.34594 10.8824 7.07456 10.8824 5.74887V4.49925C10.8824 3.83641 11.1426 3.20071 11.606 2.73201C12.0693 2.26331 12.6977 2 13.3529 2C14.0082 2 14.6366 2.26331 15.0999 2.73201C15.5632 3.20071 15.8235 3.83641 15.8235 4.49925V10.7474H19.5294C20.1847 10.7474 20.8131 11.0107 21.2764 11.4794C21.7397 11.9481 22 12.5838 22 13.2466L20.7647 19.4947C20.5871 20.2613 20.25 20.9196 19.8045 21.3704C19.3589 21.8211 18.8288 22.04 18.2941 21.994H9.64706C8.6642 21.994 7.72159 21.599 7.0266 20.896C6.33162 20.1929 5.94118 19.2394 5.94118 18.2451" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
-        <strong> Berhasil Validasi!</strong> {{ resultValidate.total - resultValidate.error }} data berhasil <u>di verifikasi</u>, {{ resultValidate.error }} data duplikat ditemukan (akan dihapus).
+        <strong> Berhasil Validasi!</strong> {{ resultValidate.total - resultValidate.error }} data berhasil <u>di verifikasi</u>{{ resultValidate.error != 0 ? `, ${resultValidate.error} data duplikat ditemukan (akan dihapus).` : `. `}}.
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     </b-col>
@@ -161,8 +161,8 @@
                     <span v-if="result.is_verification == false" class="badge bg-light border border-gray text-gray rounded-pill py-1 px-3"> Belum Valid </span>
                   </td>
                   <td class="d-flex justify-content-center">
-                    <button class="btn btn-primary btn-sm" @click="insertSelected(result)">Pilih</button>
-                    <button class="btn btn-success btn-sm ms-2" @click="processSingleValidate(result.id)">Validasi</button>
+                    <button :disabled="result.is_verification == true" class="btn btn-primary btn-sm" @click="insertSelected(result)">Pilih</button>
+                    <button :disabled="result.is_verification == true" class="btn btn-success btn-sm ms-2" @click="processSingleValidate(result.id)">Validasi</button>
                   </td>
                 </tr>
               </tbody>
@@ -302,15 +302,17 @@ export default {
         }
       }
     },
-    cariData: debounce(async function (showAll, page) {
-      this.resultMode = showAll;
-      
+    cariData: debounce(async function (showAll, page) {      
       if(showAll == false && !this.inputName) {
         return
       }
 
       if(showAll == false && this.inputName.length < 2) {
         return
+      }
+
+      if(showAll == false && !page) {
+        this.resetSearch();
       }
 
       if(!page) {
@@ -331,12 +333,12 @@ export default {
           queryParam += `&tps=${this.selectedTps}`;
           queryParam += `&limit=${limit}`;
           queryParam += `&offset=${offset}`;
-
-      console.log(queryParam);
-
+      
       if(showAll == false) {
         queryParam += `&nama=${this.inputName}`;
       }
+
+      console.log(queryParam);
 
       const response = await axios.get(`${process.env.VUE_APP_BACKEND_API}/api/v1/resident/list?${queryParam}`, withHeader);
 
@@ -352,6 +354,7 @@ export default {
 
       this.resultPagination.currentLimit = limit;
       this.resultPagination.currentOffset = offset;
+      this.resultMode = showAll;
 
       setTimeout(() => {
         this.isOnFetch = false;
@@ -454,6 +457,8 @@ export default {
             error: totalError
           }
 
+          this.cariData(this.resultMode, this.resultPagination.currentPage);
+
           setTimeout(() => {
             this.succesValidate = false;
           }, 10000);
@@ -499,6 +504,8 @@ export default {
             total: totalValidate,
             error: totalError
           }
+
+          this.cariData(this.resultMode, this.resultPagination.currentPage);
 
           setTimeout(() => {
             this.succesValidate = false;
